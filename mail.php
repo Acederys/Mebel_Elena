3aaa6284292ceaf87da2a41a7eb196e043bf34d5
Alexandra <acederys@gmail.com>
2018-09-14 22:51:34 +0300

    сделан блок портфолио, подвал

---
+++ mail.php
@@ -0,0 +1,43 @@
+<?php
+require 'phpmailer/PHPMailerAutoload.php';
+
+$username = $_POST['username'];
+$phone = $_POST['phone'];
+$user_message = $_POST['user_message'];
+
+$mail = new PHPMailer;
+
+$mail->CharSet = 'utf-8';
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP(); // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'acederys@mail.ru';                 // SMTP username
+$mail->Password = '2ariadycys2ariaducus';                           // SMTP password
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+
+$mail->setFrom('acederys@mail.ru', 'Ildar S. Zhiganshin');
+$mail->addAddress('acederys@mail.ru', 'Joe User');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Это сабджект сообщения';
+$mail->Body    = 'Это тело сообщения дальше жирное <b>блаблабла</b>';
+$mail->AltBody = 'Это сообщение в формате plain text';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+
+?>
